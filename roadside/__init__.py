0f5efdeba99f0ffb1d9ff4e953e74c40fdf72b0d
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-08-13 13:30:33 +0800

    feat: activate V0.6.12 track stale cleanup

--- roadside/__init__.py
+++ roadside/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.2.0"
+__version__ = "0.6.12"
+
+from .track_stale_cleanup import install_stale_cleanup_patch
+
+install_stale_cleanup_patch()
+print("RoadsideStation V0.6.12 Track Stale Cleanup policy active")
