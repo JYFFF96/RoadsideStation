51a7b2b2d65c3164c08e4926420ba23212aab2b6
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-08-13 13:44:45 +0800

    feat: activate V0.6.12.1 far geometry stability diagnostics

--- roadside/__init__.py
+++ roadside/__init__.py
@@ -1,6 +1,8 @@
-__version__ = "0.6.12"
+__version__ = "0.6.12.1"
 
 from .track_stale_cleanup import install_stale_cleanup_patch
+from .far_geometry_stability import install_far_geometry_stability_patch
 
 install_stale_cleanup_patch()
-print("RoadsideStation V0.6.12 Track Stale Cleanup policy active")
+install_far_geometry_stability_patch()
+print("RoadsideStation V0.6.12.1 Far Geometry Stability Diagnostics active")
