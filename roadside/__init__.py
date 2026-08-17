3f6126170a2d6b93828b9baf032566c4af328899
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-08-17 10:30:37 +0800

    feat: refine detector distance calibration shadows

--- roadside/__init__.py
+++ roadside/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "0.6.12.8.2.2.38"
+__version__ = "0.6.12.8.2.2.39"
 
 from .track_stale_cleanup import install_stale_cleanup_patch
 from .far_geometry_stability import install_far_geometry_stability_patch
 
 install_stale_cleanup_patch()
 install_far_geometry_stability_patch()
-print("RoadsideStation V0.6.12.8.2.2.38 Detector Association Calibration Shadow active")
+print("RoadsideStation V0.6.12.8.2.2.39 Fine Distance Calibration Shadow active")
