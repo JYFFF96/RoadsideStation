7973d64cc3dae28be0543252d960a2f49894608d
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-08-17 11:46:57 +0800

    V0.6.12.8.2.2.48: add V2X AVW and SLW event foundation

--- roadside/__init__.py
+++ roadside/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "0.6.12.8.2.2.47"
+__version__ = "0.6.12.8.2.2.48"
 
 from .track_stale_cleanup import install_stale_cleanup_patch
 from .far_geometry_stability import install_far_geometry_stability_patch
 
 install_stale_cleanup_patch()
 install_far_geometry_stability_patch()
-print("RoadsideStation V0.6.12.8.2.2.47 Camera Projection-Rejection Attribution active")
+print("RoadsideStation V0.6.12.8.2.2.48 V2X Event Foundation active")
