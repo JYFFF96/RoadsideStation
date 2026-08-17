beab44a979c99310d792235c775bacb6eba0cb35
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-08-17 11:06:36 +0800

    V0.6.12.8.2.2.43: add opt-in aligned sensor snapshots

--- roadside/__init__.py
+++ roadside/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "0.6.12.8.2.2.42"
+__version__ = "0.6.12.8.2.2.43"
 
 from .track_stale_cleanup import install_stale_cleanup_patch
 from .far_geometry_stability import install_far_geometry_stability_patch
 
 install_stale_cleanup_patch()
 install_far_geometry_stability_patch()
-print("RoadsideStation V0.6.12.8.2.2.42 Sensor Timestamp Skew Shadow active")
+print("RoadsideStation V0.6.12.8.2.2.43 Timestamp-Aligned Snapshot Benchmark active")
